dc0d40203ba1ffb23bfea8283b336bbdd123995e
Tani-7777 <saxenatanishq4567@gmail.com>
2025-11-07 12:03:37 +0530

    install axios and cors with credential to save token

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,52 @@
+import axios from "axios";
+import { useState } from "react";
+
 const Login = () => {
-  return <div className="text-9xl">Login</div>;
+  const [emailId, setEmailId] = useState("tani@gmail.com");
+  const [password, setPassword] = useState("Tan@1234");
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:9999/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  return (
+    <div className="flex justify-center items-center my-30">
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <legend className="fieldset-legend">Login</legend>
+
+        <label className="label">Email</label>
+        <input
+          type="email"
+          value={emailId}
+          onChange={(e) => setEmailId(e.target.value)}
+          className="input"
+          placeholder="Email"
+        />
+
+        <label className="label">Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="input"
+          placeholder="Password"
+        />
+
+        <button className="btn btn-neutral mt-4" onClick={handleLogin}>
+          Login
+        </button>
+      </fieldset>
+    </div>
+  );
 };
 
 export default Login;
